c98bddd68faca3660b14bb83a2827928d9646c75
Vic <gandergoku@gmail.com>
2026-08-04 21:46:02 -0400

    Preparar aplicación para despliegue en Render

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>¡Hola Mundo desde JavaScript!</h1>");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
